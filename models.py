327afe1d68a60bc64ec4d77807e78c08f69d4880
drw <drw@users.noreply.github.com>
2018-02-13 17:17:18 -0500

    Add more models and fields, including foreign-key relations

--- models.py
+++ models.py
@@ -1,30 +1,62 @@
 from django.db import models
 
 class Municipality(models.Model):
-    municipality = models.CharField(max_length=100, unique=True, editable=True)
-    cog	= models.CharField(max_length=100, editable=True)
-    congressional_district = models.SmallIntegerField(editable=True)
-    municipal_web_site = models.CharField(max_length=100, blank=True, null=True, editable=True)
+    municipality = models.CharField(max_length=100, unique=True)
+    cog = models.CharField(verbose_name = "COG", max_length=100)
+    congressional_district = models.SmallIntegerField()
+    municipal_web_site = models.CharField(max_length=100, blank=True, null=True)
+    mayor = models.CharField(max_length=100, blank=True, null=True)
+    manager_or_secretary = models.CharField(max_length=100, blank=True, null=True)
+    manager_contact = models.CharField(max_length=200, blank=True, null=True)
+    school_district = models.CharField(max_length=100, blank=True, null=True)
+    # [ ] School district could also be a separate table.
+    
+    
+    
+    # [ ] state_house_representative, state_house_representative_party, state_senator, and state_senator_party need to be linked other tables through the district numbers
+
+
+    class Meta:
+        verbose_name_plural = "municipalities"
 
 class CouncilMember(models.Model):
-    name = models.CharField(max_length=100, editable=True)
+    name = models.CharField(max_length=100)
+    municipality = models.ForeignKey(Municipality)
+
+class StateHouseDistrict(models.Model):
+    district = models.SmallIntegerField()
+    municipality = models.ForeignKey(Municipality)
+
+class StateSenateDistrict(models.Model):
+    district = models.SmallIntegerField()
     municipality = models.ForeignKey(Municipality)
 
 class FireDepartment(models.Model):
-    name = models.CharField(max_length=100, unique=True, editable=True)
-    street_address = models.CharField(max_length=100, editable=True)
-    city = models.CharField(max_length=50, editable=True)
-    zip_code = models.CharField(max_length=10, editable=True)
-    latitude = models.FloatField(blank=True, null=True, editable=True)
-    longitude = models.FloatField(blank=True, null=True, editable=True)
+    name = models.CharField(max_length=100, unique=True)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
 
 class PoliceDepartment(models.Model):
-    police_station = models.CharField(max_length=100, unique=True, editable=True)
-    street_address = models.CharField(max_length=100, editable=True)
-    city = models.CharField(max_length=50, editable=True)
-    zip_code = models.CharField(max_length=10, editable=True)
-    chief_name = models.CharField(max_length=50, blank=True, null=True, editable=True)
-    phone = models.CharField(max_length=50, blank=True, null=True, editable=True)
+    police_station = models.CharField(max_length=100, unique=True)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    chief_name = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
     chief_email = models.CharField(max_length=90, blank=True, null=True, editable=True)
-    web_site = models.CharField(max_length=90, blank=True, null=True, editable=True)
+    web_site = models.CharField(max_length=90, blank=True, null=True)
 
+class EMSDepartment(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    director_name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    director_email = models.CharField(max_length=100, blank=True, null=True)
+    web_site = models.CharField(max_length=90, blank=True, null=True)
+
+    municipality = models.ForeignKey(Municipality)
