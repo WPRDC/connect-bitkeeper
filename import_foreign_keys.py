acaa8db050cd0c6dcc5563455c5f3922d96054de
drw <drw@users.noreply.github.com>
2018-03-09 10:11:45 -0500

    Migrate to link_things function to correctly handle multi-item cell imports

--- import_foreign_keys.py
+++ import_foreign_keys.py
@@ -56,9 +56,11 @@ with open('bitkeeper/data/municipality.csv') as csvfile:
         print(row['municipality'])
         municipality = Municipality.objects.get(municipality=row['municipality'])
         #print(row['ems_department'])
-        link_foreign_key(row['ems_department'],EMSDepartment,'name',municipality,'ems_department')
-        link_foreign_key(row['fire_department'],FireDepartment,'name',municipality,'fire_department')
-        link_foreign_key(row['police_station'],PoliceDepartment,'police_station',municipality,'police_department')
+        link_things(string_to_list(row['ems_department']),EMSDepartment,'name',municipality,'ems_department')
+        #link_foreign_key(row['ems_department'],EMSDepartment,'name',municipality,'ems_department')
+        link_things(string_to_list(row['fire_department']),FireDepartment,'name',municipality,'fire_department')
+        link_things(string_to_list(row['police_station']),PoliceDepartment,'police_station',municipality,'police_department')
+        #link_foreign_key(row['police_station'],PoliceDepartment,'police_station',municipality,'police_department')
         #try:
         #    ems = EMSDepartment.objects.get(name=row['ems_department'])
         #    if ems is not None:
