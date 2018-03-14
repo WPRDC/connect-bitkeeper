805c480e9d746bc7a7468702022e280136d24125
drw <drw@users.noreply.github.com>
2018-03-14 14:47:22 -0400

    Fix case of generated CKAN resource names

--- views.py
+++ views.py
@@ -265,7 +265,7 @@ def export_table_to_ckan(request,table_name):
     pprint(ckan_fields)
     pprint(primary_keys)
     #message = 'Edit code to send data'
-    resource_name_base = target_table._meta.verbose_name_plural.title()
+    resource_name_base = target_table._meta.verbose_name_plural
     message = send_data_to_pipeline(resource_name_base,table_name,schema,list_of_dicts,ckan_fields,primary_keys,chunk_size=5000)
 
 
